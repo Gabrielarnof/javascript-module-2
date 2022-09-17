2cc7a19c80b106c6b125aee59190d123d8aac9d2
Gabriel Arno <agafarno@gmail.com>
2022-09-17 09:52:46 +0200

    homework moduel 1

--- week-1/Homework/dom-manipulation-1/exercise.js
+++ week-1/Homework/dom-manipulation-1/exercise.js
@@ -4,17 +4,24 @@ Task 1
 Write JavaScript below that logs:
     1. all the "p" element nodes of the document,
     --> should log a list of nodes with a length of 6
-
+*/
+const pEl = document.querySelectorAll("p");
+console.log(pEl);
+/*
     2. the first div element node
     --> should log the ".site-header" node
-
+*/
+const siteHeader = document.querySelector("div");
+console.log(siteHeader);
+/*
     3. the element with id "jumbotron-text"
     --> should log the "#jumbotron-text" node
-
+*/
+/*
     4. all the "p" elements of contained inside  the .primary-content element node
     --> should log a list of nodes with a length of 3
-
 */
+
 
 
 /*
