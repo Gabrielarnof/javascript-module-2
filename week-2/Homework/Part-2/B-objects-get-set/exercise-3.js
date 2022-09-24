58d209e09e9d55b6575d34cf0295c8e588aadb59
Gabriel Arno <agafarno@gmail.com>
2022-09-24 14:02:58 +0200

    week2

--- week-2/Homework/Part-2/B-objects-get-set/exercise-3.js
+++ week-2/Homework/Part-2/B-objects-get-set/exercise-3.js
@@ -1,9 +1,8 @@
-/*
-    Write code in the space provided so that it outputs "Gilbert"
-*/
-
 // WRITE CODE HERE
-
+var kitten = {
+    name: "Gilbert",
+    color: "brown"
+}
 
 console.log(kitten.name);
 
